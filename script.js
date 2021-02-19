1f75057fd21adc50b4337ee4165da5bf204c187c
Meerim Toktorova <meritoktorova@mail.com>
2021-02-19 16:59:44 +0600

    added let

--- script.js
+++ script.js
@@ -1,8 +1,36 @@
-let name = document.querySelector('#name');
-let message = document.querySelector('#message');
-let color = document.querySelector('#color');
-let button = document.querySelector('#button');
 
-button.addEventListener('click', function(){
-    console.log(input.value);
+
+
+let btn1 = document.querySelector('button')
+let nameSelector = document.querySelector('#name')
+let massege = document.querySelector('#message')
+let color = document.querySelector('#color')
+let chat = document.querySelector('#chat')
+
+btn1.addEventListener("mousedown", function(){
+    let div = document.createElement('div')
+    let span1 = document.createElement('span')
+    let span2 = document.createElement('span')
+    div.append(span1)
+    div.append(span2)
+    chat.append(div)
+    span1.innerText = nameSelector.value + ": "
+    span2.innerText = massege.value
+    span1.style.color = color.value
+
+
+    fetch("https://it-academy-ajax-table-21e1b-default-rtdb.firebaseio.com/chat.json",{
+        method: "POST",
+        body: JSON.stringify({
+            name: nameSelector.value,
+            massage:  massege.value,
+            color: color.value
+        })
+
+    })
+
+    
 })
+
+
+
