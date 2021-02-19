f0836a24cf33533746020f281d58c662dc71be0f
Meerim Toktorova <meritoktorova@mail.com>
2021-02-19 16:25:40 +0600

    added let

---
+++ script.js
@@ -0,0 +1,8 @@
+let name = document.querySelector('#name');
+let message = document.querySelector('#message');
+let color = document.querySelector('#color');
+let button = document.querySelector('#button');
+
+button.addEventListener('click', function(){
+    console.log(input.value);
+})
